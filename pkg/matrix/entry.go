0ebc1d483c439b1d52a30365bff478876ddf9d7d
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2025-07-12 09:59:43 +0200

    Store task finish time in DateEnd field
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- pkg/matrix/entry.go
+++ pkg/matrix/entry.go
@@ -77,7 +77,7 @@ func (e *entry) Execute(mc *MatrixConfig, roomID id.RoomID, src *structs.Source)
 
 		de.IsTodo = true
 		de.IsDone = true
-		de.SetMetadata("finished", humantime.Now().FormatDate())
+		de.DateEnd = *humantime.Now()
 
 		if err := mc.App.UpdateEntry(de); err != nil {
 			mc.App.Logger().Error("Failed to update entry", "error", err)
